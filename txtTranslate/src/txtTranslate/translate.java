780695edb0e1bc9ca243dd3926e6f0692164fdea
latte3cup <latte3cup@naver.com>
2023-07-12 23:32:55 +0900

    File Reader added

--- txtTranslate/src/txtTranslate/translate.java
+++ txtTranslate/src/txtTranslate/translate.java
@@ -1,8 +1,30 @@
 package txtTranslate;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class translate {
 
 	public static void main(String[] args) {
+		 File file = new File("path/to/your/file.txt");
+		try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Process the line here
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	
  
 		
