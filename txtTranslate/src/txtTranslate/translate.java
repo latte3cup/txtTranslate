b6648f34ca09dd891d19d06d526d66b86324b646
latte3cup <latte3cup@naver.com>
2023-07-13 23:29:49 +0900

    added translation module

--- txtTranslate/src/txtTranslate/translate.java
+++ txtTranslate/src/txtTranslate/translate.java
@@ -7,24 +7,20 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class translate {
-
 	public static void main(String[] args) {
-		 File file = new File("path/to/your/file.txt");
-		try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                // Process the line here
-                System.out.println(line);
-            }
-
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		String projectDirectory = System.getProperty("user.dir");
+		File input = new File(projectDirectory+"/Input");
+		
+		File[] files = input.listFiles();
+		if (files != null) {
+		    for (File file : files) {
+		        if (file.isFile()) {
+		            System.out.println(file.getName());
+		        }
+		    }
+		}else {
+			System.out.println("There is no file");
+		}
 	
  
 		
